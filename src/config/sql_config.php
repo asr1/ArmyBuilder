24a385afdd510f297e0620988c9aaa89fd561443
alrine <alrine@ps623981.dreamhostps.com>
2020-07-12 11:02:36 -0700

    Add config template

---
+++ src/config/sql_config.php
@@ -0,0 +1,7 @@
+<?php 
+$DB_HOST = 'localhost or domain';
+$DB_USER = 'username';
+$DB_PASS = 'password';
+$DB_NAME = 'armybuilder';
+$mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+?>
