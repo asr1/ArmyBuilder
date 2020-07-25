3bc5877216402d32781f386203b7630cdd238e75
Alex Rinehart <alexandersrinehart@gmail.com>
2020-07-24 19:48:20 -0700

    Fix unit cost, add text abilities

--- src/php/getGearForUnit.php
+++ src/php/getGearForUnit.php
@@ -2,8 +2,7 @@
 include('../config/sql_config.php');
 
 $unitId = $_GET['unitId'];
- 
-$query =  $mysqli->prepare("select * from unit_to_gear left join gear on gear.id = gearId where unitId=?");
+$query =  $mysqli->prepare("select * from unit_to_gear left join gear on gear.id = unit_to_gear.gearId left join gear_to_abilities on gear.id = gear_to_abilities.gearId left join gear_abilities on gear_abilities.id=abilityId where unitId=?");
 $query->bind_param("i", $unitId);
 $query->execute();
 $result = $query->get_result();
